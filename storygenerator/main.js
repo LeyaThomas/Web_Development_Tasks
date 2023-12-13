4e33de411cbb90c35dce0995ca2ecdeb997567bd
Leya Thomas <96945948+LeyaThomas@users.noreply.github.com>
2023-12-14 00:24:02 +0530

    Update main.js

--- storygenerator/main.js
+++ storygenerator/main.js
@@ -46,8 +46,10 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300*0.0714286)+ ' stone';
+    const temperature =  Math.round((94 - 32) * 5 / 9) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit',temperature);
+    newStory = newStory.replace('300 pounds',weight);
 
   }
 
